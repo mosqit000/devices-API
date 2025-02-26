33670b4396d006d40637ec93c8ca365b84b29550
Amer Jaradeh <awj1995@gmail.com>
2025-02-26 17:33:05 +0100

    Initail commit

---
+++ src/main/java/com/example/devicesapi/DevicesapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.devicesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devicesapi/DevicesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
